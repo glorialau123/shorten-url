20607971dcd36b823e6da898fd3607e4e9c55d77
Gloria Lau <glorialau@live.ca>
2024-05-29 22:28:54 -0700

    initialize url shorten site

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import "./HomePage.scss";
+import whale from "../assets/images/whale_1280.png";
+
+function HomePage() {
+  return (
+    <div className="home">
+      <section className="home__hero">
+        <h1 className="home__heading">WHALE TAILS</h1>
+        <h2 className="home__subheading">Shorten your URL</h2>
+        <img src={whale} alt="banner of a whale" className="home__banner" />
+      </section>
+    </div>
+  );
+}
+
+export default HomePage;
