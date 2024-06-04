ebb2bafe039f7a1cbe75c5791cd1c34415f4cdc3
Gloria Lau <glorialau@live.ca>
2024-06-03 19:08:01 -0700

    change input component to validate complete link via regex

--- src/components/UrlLink/UrlLink.jsx
+++ src/components/UrlLink/UrlLink.jsx
@@ -3,12 +3,19 @@ import "./UrlLink.scss";
 function UrlLink(props) {
   const { savedUrlInSessionStorage, shortUrlInSessionStorage, copiedStatus, onCopy } =
     props;
+  const isLoading = !savedUrlInSessionStorage || !shortUrlInSessionStorage;
 
   return (
     <div className="links">
       <div className="links__area">
-        <p className="links__original">{savedUrlInSessionStorage}</p>
-        <p className="links__shortened">{shortUrlInSessionStorage}</p>
+        {isLoading ? (
+          <p className="links__loading">Loading</p>
+        ) : (
+          <>
+            <p className="links__original">{savedUrlInSessionStorage}</p>
+            <p className="links__shortened">{shortUrlInSessionStorage}</p>
+          </>
+        )}
       </div>
       <button
         className={`links__button ${copiedStatus ? "links__button--clicked" : ""}`}
